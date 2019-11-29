ad46d260ce9c2c286092fc7874ae848cc5cb7e9f
Ken Harmon <harmkenn@gmail.com>
2019-11-29 11:16:22 +0300

    The function the converts lat long to mgrs is now near perfect. I am going to work on converting the other direction now.

--- Artillery/LL2UTH2MGRS.R
+++ Artillery/LL2UTH2MGRS.R
@@ -3,6 +3,7 @@ library(rgdal)
 library(mgrs)
 library(tidyverse)
 library(numbers)
+library(numform)
 
 nzones <- c("C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X")
 nnzone <- c(3:22)
@@ -21,7 +22,7 @@ LL2UTM2mgrs<-function(lng,lat){
   LL <- data.frame(LNG = lng, LAT = lat)
   zone <- floor(1/6*lng)+31
   nz <- data.frame("nnzone" = floor(1/8*lat)+13)
-  lz <- left_join(nz,ntlzones)
+  lz <- left_join(nz,ntlzones,by = "nnzone")
   coordinates(LL) <- c("LNG", "LAT")
   proj4string(LL) <- CRS("+proj=longlat +datum=WGS84")  ## for example
   UTM <- spTransform(LL, CRS(paste("+proj=utm +zone=",zone," ellps=WGS84",sep='')))
@@ -31,24 +32,39 @@ LL2UTM2mgrs<-function(lng,lat){
   esql <- esqls[emod,esq]
   east <- plyr::round_any(UTM@coords[1],1) - plyr::round_any(UTM@coords[1],100000,floor)
   nsq <-  plyr::round_any(UTM@coords[2],100000,floor)/100000
+  if (nsq < 0) {nsq <- 100+nsq}
+  if((zone %% 2) == 0) {nsq <- nsq +5}
   nsql <- nsqls[nsq]
   north <- plyr::round_any(UTM@coords[2],1) - plyr::round_any(UTM@coords[2],100000,floor)
-  final <- as.data.frame(c(LL,UTM, paste(zone,lz[1,2],esql,nsql,east,north, sep = "")))
-  colnames(final) <- c("Long","Lat","Easting","Northing","MGRS")
+  easting <- f_pad_zero(round(UTM@coords[1],0), width = 6, pad.char = "0")
+  northing <- round(UTM@coords[2],0)
+  if (northing < 0){northing <- 10000000 + northing}
+  northing <- f_pad_zero(northing, width = 7, pad.char = "0")
+  zone <- f_pad_zero(zone, width = 2, pad.char = "0")
+  final <- as.data.frame(c(LL, easting,northing,paste(zone,lz[1,2], sep = ""), 
+              paste(zone,lz[1,2],esql,nsql,f_pad_zero(east, width = 5, pad.char = "0"),
+                    f_pad_zero(north, width = 5, pad.char = "0"), sep = "")))
+  colnames(final) <- c("Long","Lat","Easting","Northing","Gridzone","MGRS")
   return(final)
 }
 
-LL2UTM2mgrs(-120,50)
+## Here is the function that turns
+
+LL2UTM2mgrs(-138.23,-34.55)
 
 longv <- (sample.int(36000,size=20,replace=TRUE)-1)/100-180
-latv <- (sample.int(9000,size=20,replace=TRUE)-1)/100-90
+latv <- (sample.int(16000,size=20,replace=TRUE)-1)/100-80
 samplell <- data.frame(cbind(longv,latv))
 
+
+samplell$UTME <- NA
+samplell$UTMN <- NA
 samplell$mgrs <- NA
+samplell$check <- NA
 
 for (i in 1:nrow(samplell)){
-  samplell$mgrs[i] <- as.matrix(LL2UTM2mgrs(samplell$longv[i],samplell$latv[i]))[5]
+  samplell$UTME[i] <- as.matrix(LL2UTM2mgrs(samplell$longv[i],samplell$latv[i]))[3]
+  samplell$UTMN[i] <- as.matrix(LL2UTM2mgrs(samplell$longv[i],samplell$latv[i]))[4]
+  samplell$mgrs[i] <- as.matrix(LL2UTM2mgrs(samplell$longv[i],samplell$latv[i]))[6]
+  samplell$check[i] <- mgrs::latlng_to_mgrs(samplell$latv[i],samplell$longv[i])
 }
-
-
-

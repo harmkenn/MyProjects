1f307340560f561cd1e6e54e28c89b28a0687a37
K. Scott Harmon <harmkenn@gmail.com>
2019-12-08 09:40:53 +0000

    Deflection is now working with my EarthPoints

---
+++ Artillery/Shiny_Shots/app.R
@@ -0,0 +1,122 @@
+#library(rsconnect)
+#library(tidyverse)
+library(shiny)
+#library(ggmap)
+#library(mgrs)
+#library(geosphere)
+#library(sp)
+library(leaflet)
+library(EarthPoints)
+
+
+ui <- fluidPage(
+    titlePanel("Shots by Ken Harmon", windowTitle = "Shots"),
+    tabsetPanel(
+        tabPanel("Deflection",
+                 sidebarLayout(
+                     sidebarPanel(
+                         radioButtons(inputId = "maptype",label = "Pick a Map Type",choices = 
+                                          c("Road Map"="OpenStreetMap.Mapnik",
+                                            "Terrain"="Stamen.Terrain",
+                                            "Satellite"="Esri.WorldImagery",
+                                            "Night Lights" = "NASAGIBS.ViirsEarthAtNight2012")),
+                         textInput("MGRS","Center MGRS", "11TNH7000080000"),
+                         numericInput("aof","Azimuth of Fire",2000,min = 0,max = 6399),
+                         actionButton(inputId = "get_map", label = "Get Map"),
+                         textOutput("Target"),
+                         textOutput("dist"),
+                         textOutput("az"),
+                         textOutput("defl"),
+                         textInput("lookup","MGRS lookup"),
+                         actionButton(inputId = "get_mgrs", label = "Get Grid"),
+                         textOutput("lMGRS"),
+                         textOutput("lLngLat")
+                     ),
+                     mainPanel(
+                         leafletOutput("mymap", width = "600px", height = "600px")
+                     )
+                 )
+        ),
+        tabPanel("Table", tableOutput("table")
+        )
+    )
+)
+server <- function(input, output) {
+    
+    output$mymap <- renderLeaflet({
+        if (input$get_map == 0) 
+            return()
+        isolate({
+            mgrs <- MGRS2UTM2LL(input$MGRS)
+            clat <- as.numeric(as.vector(mgrs[1,6]))
+            clng <- as.numeric(as.vector(mgrs[1,7]))
+            aof <- input$aof
+            aoftip <- polar_ll(clat,clng,30,input$aof/6400*360)
+            aoftiplat <- as.numeric(as.vector(aoftip[1,5]))
+            aoftiplng <- as.numeric(as.vector(aoftip[1,6]))
+            aofdf <- data.frame(lng = c(clng,aoftiplng),lat = c(clat,aoftiplat))
+            
+            
+            m <- leaflet() %>% 
+                setView(clng,clat,zoom=10) %>%
+                addProviderTiles(input$maptype) %>%
+                addCircleMarkers(lng = clng, lat = clat, radius = 5) %>%
+                addPolylines(data = aofdf, ~lng, ~lat, group = "aof", 
+                             color = "orange")
+        })
+    })
+    observeEvent(input$get_mgrs, {
+        output$lMGRS <- renderText({paste(" ")})
+        output$lLngLat <- renderText({paste(" ")})
+        register_google(key = "AIzaSyAfnLNZjvYdMx-cyga_qA1oJ6P36dRGalA")
+        lLatLon <- geocode(input$lookup)
+        llng <- lLatLon$lon
+        llat <- lLatLon$lat
+        lmgrs <- mgrs::latlng_to_mgrs(llat,llng)
+        output$lMGRS <- renderText({paste("MGRS: ", lmgrs)})
+        output$lLngLat <- renderText({paste("Longitude: ", llng, 
+                                            "Latitude: ", llat)})
+    })
+    observeEvent(input$mymap_click, {
+        click <- input$mymap_click
+        tlng <- click$lng
+        tlat <- click$lat
+        mgrs <- MGRS2UTM2LL(input$MGRS)
+        clat <- as.numeric(as.vector(mgrs[1,6]))
+        clng <- as.numeric(as.vector(mgrs[1,7]))
+        text<-paste("Latitude ", round(clat,2), "Longtitude ",
+                    round(clng,2))
+        
+        output$Target <-  renderText({paste("Target: ", 
+                                            LL2UTM2mgrs(clat,clng)[1,7])})
+        
+        shot <- shot_ll(clat,clng,tlat,tlng)
+        output$dist <- renderText({paste("Distance to Target: ", shot[1,5])})
+        
+        aof <- input$aof
+        
+        az <- as.numeric(as.vector(shot[1,6]))*6400/360
+        if (az<0) {az <- az + 6400}
+        az <- round(az,1)
+        output$az <- renderText({paste("Azimuth to Target: ", az, "mils")})
+        
+        defl <- 3200+(aof-az)
+        if (defl < 0) {defl <- defl + 6400}
+        output$defl <- renderText({paste("Deflection to Target: ", defl, "mils")})
+        
+        proxy <- leafletProxy("mymap")
+        
+        pointdf <- data.frame(lng = c(clng,tlng),lat = c(clat,tlat))
+        ## This displays the pin drop circle
+        
+        proxy %>% 
+            clearGroup("new_point") %>%
+            #clearMarkers(layerId=input$mymap_click$id) %>%
+            #addPopups(click$lng, click$lat) %>%
+            addCircles(click$lng, click$lat, radius=100, color="red", group = 
+                           "new_point") %>%
+            addPolylines(data = pointdf, ~lng, ~lat, group = "new_point")
+    })
+}
+
+shinyApp(ui, server)

de5daf5d96960ceeb297acb873d07bc2965e62f7
K. Scott Harmon <harmkenn@gmail.com>
2020-01-07 06:22:58 +0000

    Working on Dataset tab from cloud to desktop.

--- ShinyStatTools/datasets.R
+++ ShinyStatTools/datasets.R
@@ -1,14 +1,14 @@
-library(xlsx)
+#library(xlsx)
 #file.list <- list.files(pattern='*.*')
 #df.list <- lapply(file.list, read_excel)
-library(plyr)
-df.list <- lapply(file.list, function(x) read.xlsx(file=x, sheetIndex=1,
-                  colIndex=1:4,as.data.frame=TRUE, header=FALSE, FILENAMEVAR=x))
-final.df <- rbind.fill(df.list)
+#library(plyr)
+#df.list <- lapply(file.list, function(x) read.xlsx(file=x, sheetIndex=1,
+#                  colIndex=1:4,as.data.frame=TRUE, header=FALSE, FILENAMEVAR=x))
+#final.df <- rbind.fill(df.list)
 
-#save(list = ls(.GlobalEnv), file = "data.Rdata")
+#save(list = ls(.GlobalEnv), file = "BYUIdata.Rdata")
 
-load("/cloud/project/ShinyStatTools/datasetsrda/data.Rdata")
+#load("/cloud/project/ShinyStatTools/BYUIdata.Rdata")
 
 dslist <- ls()[grepl('data.frame', sapply(ls(), function(x) class(get(x))))]
 
@@ -19,13 +19,13 @@ library(shiny)
 ui=fluidPage(
   selectInput("data_input",label="Select data",
               choices=dslist),
-  tableOutput("table_output")
+  rHandsontableOutput("table_output")
 )
 
 server=function(input,output) {
 
   getdata <- reactive({ get(input$data_input, .GlobalEnv) })
-  output$table_output <- renderTable({head(getdata())})
+  output$table_output <- renderRHandsontable({head(getdata())})
 }
 
     shinyApp(ui, server)
